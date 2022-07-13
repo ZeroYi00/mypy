4cfbdcfd13b60606c35196f01a21b78eb6b980d8
Zero Yi <ib_yeang@163.com>
2022-07-13 23:08:22 +0800

    线程同步&线程队列

---
+++ py-thread/study-queue.py
@@ -0,0 +1,11 @@
+# !/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# @FileName  :study-queue.py
+# @Desc      :
+# @Time      :2022/7/13 22:45
+# @Email     :ib_yeang@163.com
+# @Author    :Zero Yi
+ 
+ 
+if __name__ == '__main__':
+    print('hi python')
